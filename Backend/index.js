2971980f1d99db65db059e0a73d4110a33936e2e
ThomasHurtado <thomashurtado@outlook.com.br>
2024-04-19 12:45:04 -0300

    Avanço no codigo

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+//config JSON
+app.use(express.json())
+
+//public for images
+app.use(express.static('public'))
+
+//Routes
+const UserRoutes = require('./routes/UserRoutes')
+
+app.use('/users', UserRoutes)
+
+mongoose
+    .connect('mongodb+srv://thomashurtado:1234@cluster0.zpyeus7.mongodb.net/BancoAPI?retryWrites=true&w=majority&appName=Cluster0')
+    .then(() => {
+        console.log("Conectamos ao banco")
+        app.listen(5000)
+    })
+    .catch((err) => console.log(err))
