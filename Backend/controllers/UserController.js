1e4d2ede7194f05465f969ba143c52bcd8a493ac
ThomasHurtado <thomashurtado@outlook.com.br>
2024-04-22 19:00:37 -0300

    Validação por Token

--- Backend/controllers/UserController.js
+++ Backend/controllers/UserController.js
@@ -1,7 +1,156 @@
 const User = require('../models/User')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 module.exports = class UserController {
      static async register(req, res) {
-        res.json('Ola')
+        const {name, email, phone, password, confirmpassword} = req.body
+
+        if(!name){
+            res.status(422).json({message: 'O nome é obrigatorio!'})
+            return
+        }
+
+        if(!email){
+            res.status(422).json({message: 'O email é obrigatorio!'})
+            return
+        }
+
+        if(!phone){
+            res.status(422).json({message: 'O telefone é obrigatorio!'})
+            return
+        }
+
+        if(!password){
+            res.status(422).json({message: 'A senha é obrigatorio!'})
+            return
+        }
+
+        if(!confirmpassword){
+            res.status(422).json({message: 'A confirmamção de senha é obrigatorio!'})
+            return
+        }
+
+        if(password !== confirmpassword){
+            res.status(422).json({message: 'As senhas devem ser iguais!'})
+            return
+        }
+
+        const userExist = await User.findOne({email: email})
+
+        if(userExist){
+            res.status(422).json({message: 'Esse email ja foi cadastrado!'})
+            return
+        }
+
+        const salt = await bcrypt.genSalt(12)
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        const user = new User({
+            name: name,
+            email: email,
+            phone: phone,
+            password : passwordHash,
+        })
+
+        try {
+            
+            const newUser = await user.save()
+            res.status(201).json({message: 'Usuario criado com sucesso', newUser})
+
+        } catch (error) {
+            res.status(500).json({message: error})
+        }
      }
+
+     static async login(req, res) {
+
+        const {email, password} = req.body
+
+        //validar
+        if(!email){
+            res.status(422).json({message: 'O email é obrigatorio!'})
+            return
+        }
+
+        if(!password){
+            res.status(422).json({message: 'A senha é obrigatorio!'})
+            return
+        }
+
+        //check user
+        const user = await User.findOne({email: email})
+
+        if(!user){
+            res.status(422).json({message: 'Usuario não cadastrado!'})
+            return
+        }
+
+        //check password
+        const checkPassword = await bcrypt.compare(password, user.password)
+
+        if(!checkPassword){
+            res.status(422).json({message: 'Senha invalida!'})
+            return
+        }
+
+        try {
+        
+        const secret = process.env.SECRET 
+        const token = jwt.sign(
+            {
+                id: user._id
+            },
+            secret,
+        )
+        res.status(200).json({message: 'Autenticação bem sucedida!', token})
+
+        } catch (error) {
+            res.status(500).json({message: error})
+        }
+     }
+
+     static async private(req, res){
+
+        const id = req.params.id
+
+        //check usuario
+        const user = await User.findById(id, '-password')
+
+        if(!user){
+            res.status(404).json({message: 'Usuario não encontrado!'})
+            return
+        }
+
+        try {
+            res.status(200).json(user)
+            
+        } catch (error) {
+            res.status(500).json({message: error})
+        }
+     }
+
+     static checkToken(req, res, next) {
+        
+        const authHeader = req.headers['authorization']
+        const token = authHeader && authHeader.split(' ')[1]
+ 
+        if(!token){
+            res.status(401).json({message: 'Acesso negado!'})
+            return
+           }
+    
+           try {
+    
+            const secret = process.env.SECRET
+    
+            jwt.verify(token, secret)
+    
+            next()
+    
+           } catch (error) {
+            res.status(400).json({message: 'Token inválido!'})
+           }
+ }
 }   
--- Backend/routes/UserRoutes.js
+++ Backend/routes/UserRoutes.js
@@ -2,7 +2,14 @@ const router = require('express').Router()
 
 const UserController = require('../controllers/UserController')
 
+//registro
 router.post('/register', UserController.register)
+
+//login
+router.post('/login', UserController.login)
+
+//rota privada
+router.get('/:id', UserController.checkToken, UserController.private)
 
 
 module.exports = router
